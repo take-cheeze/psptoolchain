98f7a6dba023a0d549712e4ecefd78b67944df2a
Takeshi Watanabe <takechi101010@gmail.com>
2011-04-09 04:51:20 +0900

    applied patch from https://sites.google.com/a/mithria.net/misaka_sn10032/top/ptc-patch

---
+++ depends/check-elf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# check-elf.sh by yreeen (yreeen@gmail.com)
+
+ ## Check for the elf library.
+ ls /usr/include/elf.h 1> /dev/null || { echo "ERROR: Install elf before continuing."; exit 1; }
---
+++ depends/check-mpc.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# check-mpc.sh by yreeen (yreeen@gmail.com)
+
+ ## Check for the mpc library.
+ ls /usr/include/mpc.h 1> /dev/null || { echo "ERROR: Install mpc before continuing."; exit 1; }
